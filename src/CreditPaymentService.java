af941d23374afa5a1753c02acc4ead6fe425523d
Yury.Fedoseev <Yri95@inbox.ru>
2021-07-01 14:59:11 +0300

    Исправление переменных, упрощение логики кода

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,11 @@
 public class CreditPaymentService {
     public double calculate(  double amount, double years) {
         double procent= 9.99; //процент по кредиту
-        double period_month = years * 12; //месяцев на выплату
+        double periodMonth = years * 12; //месяцев на выплату
         double procentmonth = procent /12 /100; //месячная процентная ставка
-        double variable = Math.pow(( 1+procentmonth),period_month); // переменная для возведения в степень  (1+ мес проц ставка)^количество платежей )
+        double x = Math.pow(( 1+procentmonth),periodMonth); // переменная для возведения в степень  (1+ мес проц ставка)^количество платежей )
         //к.а.= (мес проц ставка * (1+ мес проц ставка)^количество платежей ) / (( 1+ мес проц ставка)^колич платежей  -1)
-        double coefficient = (procentmonth * variable)/ (variable - 1); // коэффициент аннуитета
+        double coefficient = (procentmonth * x)/ (x - 1); // коэффициент аннуитета
         double payment = amount * coefficient; //платеж = сумма кредита * коэф аннуитета
 
         return payment;
